ac2a2cc1923177c1b7c997849f7f9a2c95c8b813
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-29 18:43:04 -0700

    adding middleware

--- app.js
+++ app.js
@@ -5,11 +5,35 @@ const express = require('express');
 const app = express();
 
 // Middleware
+// Allow Express (our web framework) to render HTML templates and send them back to the client using a new function
+const handlebars = require('express-handlebars');
+
+const hbs = handlebars.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo() { return 'FOO!'; },
+        bar() { return 'BAR!'; }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-});
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+})
 
 // Start Server
 
