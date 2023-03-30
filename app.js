83a942e6c2cf48b4f6eee092939c1ca2b4f9648e
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-29 18:45:21 -0700

    added search form

--- app.js
+++ app.js
@@ -20,12 +20,10 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-// Routes
+// ROUTES
 app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif', { gifUrl })
+    console.log(req.query)
+    res.render('home')
 })
 
 app.get('/greetings/:name', (req, res) => {
